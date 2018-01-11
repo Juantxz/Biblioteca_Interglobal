e13ee4ba78b7262e04fcab0ea5b312d1e1aa2bbc
Angel Aguilera <albertoangelaguilera@gmail.com>
2018-01-11 03:28:26 -0600

    QUEDAN COSAS PENDIENTES

--- buscar.js
+++ buscar.js
@@ -12,10 +12,10 @@ $(document).ready(function() {
       busqueda = $("#libro").val();
       $.getJSON('libros.json?nocache=123', function(json) {
         for (var i = 0; i < json.length; i++) {
-          if (json[i].TEMA1.includes(busqueda)) {
-            console.log("encontrado "+json[i].ID);
-              $(".contenido").append('<p>'+json[i].CLASIFICACION+'</p>')
-
+          if (json[i].TEMA1.includes(busqueda) || json[i].TITULO.includes(busqueda)) {
+            console.log("encontrado " + json[i].ID);
+            $(".contenido").append('<p>' + json[i].CLASIFICACION + '</p>')
+            $(".contenido").append('<p>' + json[i].ID + '</p>')
           }
         }
       }).fail(function(d, Status, error) {
